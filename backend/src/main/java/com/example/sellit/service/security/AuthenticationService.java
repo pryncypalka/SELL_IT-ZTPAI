b6f270020853b4d02da099806bb85a3ccd24cd36
Jacek Kamiński <jacus502@gmail.com>
2024-05-13 01:09:51 +0200

    security and JWT hot fix

--- backend/src/main/java/com/example/sellit/service/security/AuthenticationService.java
+++ backend/src/main/java/com/example/sellit/service/security/AuthenticationService.java
@@ -12,7 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
+
 
 @Service
 @RequiredArgsConstructor
