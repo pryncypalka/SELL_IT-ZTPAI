9599cb202d20f6cd9ab7a040896e91e0ddb70642
Jacek Kamiński <jacus502@gmail.com>
2024-05-13 03:26:24 +0200

    basic registration with security

--- frontend/react-app/src/components/SignUpForm.jsx
+++ frontend/react-app/src/components/SignUpForm.jsx
@@ -5,71 +5,80 @@ import axios from 'axios';
 function SignUpForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
     const [password2, setPassword2] = useState('');
-    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         // Sprawdź, czy hasła są takie same
         if (password !== password2) {
-            setMessages(['Passwords do not match']);
+            setMessage('Passwords do not match');
             return;
         }
 
         try {
-            const response = await axios.post('http://localhost:8080/api/user/addUser', {
+            const response = await axios.post('http://localhost:8080/api/v1/auth/register', {
+                username: username,
                 email: email,
-                passwordHashed: password
+                password: password
             });
 
-            if (response.status === 201) {
-                setMessages(['Registration successful']);
+            if (response.status === 200) {
+                setMessage('Registration successful');
             } else {
-                setMessages([response.data]);
+                setMessage(typeof response.data === 'object' ? response.data.error : response.data);
             }
         } catch (error) {
-            setMessages([error.message]);
+            setMessage(error.message);
         }
     };
 
     return (
         <form className={styles.form_sign_up} onSubmit={handleSubmit}>
             <div className={styles.text1}>Sign up</div>
-            <div className={styles.text2}>Create your account. It's free and only takes a minute.</div>
 
+             <div>{message}</div>
+            <div className={styles.field_name}>Your Username</div>
+            <div className={styles.input_field}>
+                <input className={styles.input_field}
+                       type="text"
+                       name="username"
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                />
 
-            {messages.map((message, index) => (
-                <div key={index}>{message}</div>
-            ))}
+            </div>
 
             <div className={styles.field_name}>Your Email</div>
             <div className={styles.input_field}>
                 <input className={styles.input_field}
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                       type="text"
+                       name="email"
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}
                 />
 
             </div>
+
             <div className={styles.field_name}>Password</div>
             <div className={styles.input_field}>
                 <input className={styles.input_field}
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                       type="password"
+                       name="password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
                 />
 
             </div>
             <div className={styles.field_name}>Confirm password</div>
             <div className={styles.input_field}>
                 <input className={styles.input_field}
-                    type="password"
-                    name="password2"
-                    value={password2}
-                    onChange={(e) => setPassword2(e.target.value)}
+                       type="password"
+                       name="password2"
+                       value={password2}
+                       onChange={(e) => setPassword2(e.target.value)}
                 />
 
             </div>
