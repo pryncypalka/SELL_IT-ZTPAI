02e75015174f653a88ae874bba5c762d3d82b600
Jacek Kamiński <jacus502@gmail.com>
2024-05-21 23:38:53 +0200

    delete allegro

--- backend/src/main/java/com/example/sellit/repository/AllegroIntegrationDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sellit.repository;
-
-import com.example.sellit.model.AllegroIntegrationData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllegroIntegrationDataRepository extends JpaRepository<AllegroIntegrationData, Long> {
-}
